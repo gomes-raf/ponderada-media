d448f0c1db6c7d558bd3d82a835b33f4441a0917
Rafael Gomes <rafael.gomes@sou.inteli.edu.br>
2025-02-10 11:45:18 -0300

    Add files via upload

---
+++ ponderada2.js
@@ -0,0 +1,25 @@
+var nota1 = 7
+console.log("Nota 1:", nota1);
+var nota2 = 5
+console.log("Nota 2:", nota2);
+var nota3 = 9
+console.log("Nota 3:", nota3);
+var média = (nota1 + nota2 + nota3) / 3;
+console.log("Média:", média);
+
+if (média >= 7) {
+    console.log("Aprovado")
+}
+
+var nota1 = 7
+console.log("Nota 1:", nota1);
+var nota2 = 6
+console.log("Nota 2:", nota2);
+var nota3 = 6
+console.log("Nota 3:", nota3);
+var média = (nota1 + nota2 + nota3) / 3;
+console.log("Média:", média);
+
+if (média < 7) {
+    console.log("Reprovado")
+}
